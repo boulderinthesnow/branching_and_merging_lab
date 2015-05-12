f96add0c84684e5d0fa670c3681645db48011d79
LindsayElia <lindsay.elia@gmail.com>
2015-05-12 15:46:31 -0700

    fourth time

--- problemSet.js
+++ problemSet.js
@@ -12,6 +12,11 @@ function frameYourPhrase(phrase) {
      Pay attention to the spacing.  The box should be a square.  In other words,
      the longest word in the phrase will dictate the size of the box.
    */  
+	
+	// one comment added by Lindsay
+	// two comment added by Lindsay
+	// three comment added by Lindsay
+	
    return "**********\n* Hello  *\n* World! *\n**********";
 }
 
